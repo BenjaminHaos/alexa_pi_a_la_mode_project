79bef0e106ef822251f6e9760a22dd29a2c6ea2c
Benjamin Haos <benjaminhaos@gmail.com>
2017-05-24 14:03:22 +0000

    mod

--- z_haos_app/alexa_av_controller.py
+++ z_haos_app/alexa_av_controller.py
@@ -26,7 +26,7 @@ logging.basicConfig(level=logging.DEBUG)
 #logging.basicConfig(level=logging.ERROR)
 #logging.basicConfig(level=logging.CRITICAL)
 
-logging.debug("Alexa AV Controller About to Start!");
+logging.debug("\nAlexa AV Controller About to Start!\n");
 
 if __name__ == "__main__" and run_alexa_listener:
     # Startup the fauxmo server
